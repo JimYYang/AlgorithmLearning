018ddb763e7ad6a48fd0a7a2b045038c7dd34215
yjs <1729135661@qq.com>
2024-03-05 15:03:03 +0800

    lc114

---
+++ LeetCode/lc114.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if (!root) return;
+        while (root)
+        {
+            auto p = root->left;
+            if (p)
+            {
+                while (p->right)
+                    p = p->right;
+                p->right = root->right;
+                root->right = root->left;
+                root->left = nullptr;
+            }
+            root = root->right;
+        }
+    }
+};
