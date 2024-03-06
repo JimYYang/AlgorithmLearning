034909c02c2e8104f55e4575e79294ae3f998628
yjs <1729135661@qq.com>
2024-03-06 09:20:12 +0800

    lc145

---
+++ LeetCode/lc145.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode*> stk;
+
+        while (root || stk.size())
+        {
+            while (root)
+            {
+                ans.push_back(root->val);
+                stk.push(root);
+                root = root->right;
+            }
+            root = stk.top()->left;
+            stk.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
