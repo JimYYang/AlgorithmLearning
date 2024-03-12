3044bf1fb7cf057dab931c6e1d2a5effd6f8da07
yjs <1729135661@qq.com>
2024-03-12 18:12:29 +0800

    lc9

---
+++ LeetCode/lc9.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        long long res = 0;
+        int tmp = x;
+        while (tmp)
+        {
+            res = res * 10 + tmp % 10;
+            tmp /= 10;
+        }
+        return res == x;
+    }
+};
