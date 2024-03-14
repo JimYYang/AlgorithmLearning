b47a8a2c6dffd340f0796f03b04a49ceaf202f6d
yjs <1729135661@qq.com>
2024-03-14 14:45:00 +0800

    lc322

---
+++ LeetCode/lc322.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int m) {
+        vector<int> f(m + 1, 1e8);
+        f[0] = 0;
+
+        for (auto &coin : coins)
+        {
+            for (int j = coin; j <= m; j++)
+            {
+                f[j] = min(f[j], f[j - coin] + 1);
+            }
+        }
+        if (f[m] == 1e8) return -1;
+        return f[m];
+    }
+};
