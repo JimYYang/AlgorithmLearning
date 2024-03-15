8044f8330713b610dc7461d64f5915801e079dd1
yjs <1729135661@qq.com>
2024-03-15 20:48:49 +0800

    lc27

---
+++ LeetCode/lc27.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int l = 0, r = 0, n = nums.size();
+        while (r < n)
+        {
+            if (nums[r] == val)
+                r++;
+            else
+            nums[l++] = nums[r++];
+        }
+        return l;
+    }
+};
