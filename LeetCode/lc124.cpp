90b2cff1d9d66be7c69e7240afa2bfa6956b5d92
JimYY <jsyang@Mac.local>
2025-01-09 10:33:59 +0800

    lc124

--- LeetCode/lc124.cpp
+++ LeetCode/lc124.cpp
@@ -12,18 +12,24 @@
 class Solution {
 public:
     int ans;
+    // 枚举路径的最高点
+    // dfs返回的是：以这个节点向下走的路径最大值（只能要么左要么右走）
+    int dfs(TreeNode *root)
+    {
+        if (!root) return 0;
+        // 用0来代表不选
+        int left = max(0, dfs(root->left));
+        int right = max(0, dfs(root->right));
+        ans = max(ans, root->val + left + right);
+        // 假设已经求出f(a) f(b)
+        // 那么u: 1). vu 2) vu + f(a) 3). vu + f(b).
+        // 用0可以很好地求出这三种情况
+        return root->val + max(left, right);
+    }
+    
     int maxPathSum(TreeNode* root) {
         ans = INT_MIN;
         dfs(root);
         return ans;
-    }
-
-    int dfs(TreeNode *root)
-    {
-        if (!root) return 0;
-        int left = max(0, dfs(root->left)), right = max(0, dfs(root->right));
-        int val = root->val;
-        ans = max(ans, val + left + right);
-        return val + max(left, right);
     }
 };
