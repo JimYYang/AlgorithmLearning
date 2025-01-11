cf02384dca58b7eeccce50e61e7b3386935a5dd2
JimYY <jsyang@Mac.local>
2025-01-11 20:48:02 +0800

    lc200

--- LeetCode/lc200.cpp
+++ LeetCode/lc200.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
-    vector<vector<char>> g;
-    int dx[4] = {-1, 0, 1, 0};
-    int dy[4] = {0, 1, 0, -1};
     int numIslands(vector<vector<char>>& grid) {
-        g = grid;
-        int ans = 0;
-        for (int i = 0; i < g.size(); i++)
-        {
-            for (int j = 0; j < g[0].size(); j++)
+        int n = grid.size(), m = grid[0].size();
+        array<int, 4> dx {-1, 0, 1, 0};
+        array<int, 4> dy {0, 1, 0, -1};
+
+        function<void(int, int)> dfs = [&](int a, int b) -> void {
+            grid[a][b] = '0';
+
+            for (int i = 0; i < 4; i++)
             {
-                if (g[i][j] == '1')
+                int x = a + dx[i];
+                int y = b + dy[i];
+
+                if (x < 0 || x >= n || y < 0 || y >= m || grid[x][y] == '0')
+                {
+                    continue;
+                }
+                dfs(x, y);
+            }
+        };
+
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (grid[i][j] == '1')
                 {
                     dfs(i, j);
                     ans++;
-                }               
+                }
             }
         }
         return ans;
-    }
-
-    void dfs(int x, int y)
-    {
-        g[x][y] = '0';
-        for (int i = 0; i < 4; i++)
-        {
-            int a = x + dx[i];
-            int b = y + dy[i];
-            if (a >= 0 && a < g.size() && b >= 0 && b < g[0].size() && g[a][b] == '1')
-            {
-                dfs(a, b);
-            }
-        }
     }
 };
