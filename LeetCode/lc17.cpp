a22f267586eb9d5e3768dbd5852a99a08ec3f540
JimYYang <jimsonnal@gmail.com>
2025-02-20 16:12:06 +0800

    lc17

--- LeetCode/lc17.cpp
+++ LeetCode/lc17.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-    vector<string> ans;
-    string digits[10]{
-        "", "", "abc", "def",
-        "ghi", "jkl", "mno",
-        "pqrs", "tuv", "wxyz"
-    };
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return {};
 
-    vector<string> letterCombinations(string s) {
-        if (s.empty()) return ans;
-        dfs(s, 0, "");
+        string values[10]{
+            "", "", "abc", "def",
+            "ghi", "jkl", "mno",
+            "pqrs", "tuv", "wxyz"
+        };
+
+        vector<string> ans;
+        function<void(int, string)> dfs = [&](int u, string path)
+        {
+            if (u == digits.size())
+            {
+                ans.emplace_back(path);
+                return;
+            }
+
+            for (auto c : values[(digits[u] - '0')])
+            {
+                dfs(u + 1, path + c);
+            }
+        };
+
+        dfs(0, "");
         return ans;
-    }
-
-    void dfs(string &s, int u, string path)
-    {
-        if (u == s.size())
-        {
-            ans.emplace_back(path);
-            return;
-        }
-        for (auto c : digits[s[u] - '0'])
-        {
-            dfs(s, u + 1, path + c);
-        }
     }
 };
