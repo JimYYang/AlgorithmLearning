7d5d3dfe1a8048b978e557016b62830cb388a713
JimYYang <jimsonnal@gmail.com>
2025-02-20 17:17:51 +0800

    lc40

--- LeetCode/lc40.cpp
+++ LeetCode/lc40.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    vector<int> path;
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        // 包含重复元素 一次
+
+        vector<vector<int>> ans;
+        vector<int> path;
+        int n = candidates.size();
+
+        function<void(int, int)> dfs = [&](int u, int left) {
+            if (left == 0)
+            {
+                ans.push_back(path);
+                return;
+            }
+
+            for (int i = u; i < n; i++)
+            {
+                if (candidates[i] > left) continue;
+                if (i > u && candidates[i] == candidates[i - 1]) continue;
+
+                path.push_back(candidates[i]);
+                dfs(i + 1, left - candidates[i]);
+                path.pop_back();
+            }
+        };
         sort(candidates.begin(), candidates.end());
-        dfs(candidates, 0, target);
+        dfs(0, target);
         return ans;
-    }
-
-    // 依次枚举每个数  看看枚举多少次
-    void dfs(vector<int> &nums, int u, int target)
-    {
-        if (!target)
-        {
-            ans.push_back(path);
-            return;
-        }
-        int n = nums.size();
-        if (u == n)
-            return;
-
-        int k = u + 1;
-        
-        while (k < n && nums[k] == nums[u]) k++;
-        int cnt = k - u;
-        int cur = nums[u];
-
-        for (int i = 0; i * cur <= target && i <= cnt; i++)
-        {
-            dfs(nums, k, target - i * cur); // 一次可能枚举多个数 跳过了好几个数
-            path.push_back(cur);
-        }
-
-        for (int i = 0; i * cur <= target && i <= cnt; i++)
-            path.pop_back();
     }
 };
