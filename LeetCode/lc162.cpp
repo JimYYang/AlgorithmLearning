f75a8b8d94859ffb85081b3088b1b5db11ffaa9b
JimYYang <jimsonnal@gmail.com>
2025-02-23 10:47:08 +0800

    lc162

--- LeetCode/lc162.cpp
+++ LeetCode/lc162.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         int l = 0, r = nums.size() - 1;
-
         while (l < r)
         {
             int mid = l + r >> 1;
+            // 注意 这里mid+1不会越界  mid=r时才会越界 这时候l==r 循环已经退出
             if (nums[mid] > nums[mid + 1]) r = mid;
-            else l = mid + 1; 
+            else l = mid + 1;
         }
         return l;
     }
--- LeetCode/lc74.cpp
+++ LeetCode/lc74.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size(), m = matrix[0].size();
-
-        int l = 0, r = n * m - 1;
+        int l = 0, r = m * n - 1;
         while (l < r)
         {
             int mid = l + r >> 1;
-            // 注意坐标转换的正确写法
-            if (matrix[mid / m][mid % m] >= target) r = mid;
+            int x = mid / m, y = mid % m;
+            if (matrix[x][y] >= target) r = mid;
             else l = mid + 1;
         }
-        return matrix[l / m][l % m] == target;
+        return matrix[l/m][l%m] == target;
     }
 };
