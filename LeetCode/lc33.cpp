e20b1bcb8bf512f85a74c876c06ddcf8ba933430
JimYYang <jimsonnal@gmail.com>
2025-02-23 11:00:51 +0800

    lc33

--- LeetCode/lc33.cpp
+++ LeetCode/lc33.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int l = 0, r = n - 1;
+        if (nums.empty()) return -1;
+        int n = nums.size(), l = 0, r = n - 1;
+
         while (l < r)
         {
-            int mid = (l + r + 1) >> 1;
+            int mid = l + r + 1 >> 1;
             if (nums[mid] >= nums[0]) l = mid;
             else r = mid - 1;
         }
 
         if (target >= nums[0]) l = 0;
-        else
-        {
-            l = r + 1;
-            r = n - 1;
-        }
-
+        else {l = r + 1, r = n - 1;}
         while (l < r)
         {
-            int mid = (l + r) >> 1;
+            int mid = l + r >> 1;
             if (nums[mid] >= target) r = mid;
             else l = mid + 1;
         }
-        if (nums[r] == target) return r;
+
+        // 【1】 0这个例子  l = r + 1的时候可能越界了
+        if (nums[r] == target) return l;
         return -1;
     }
 };
