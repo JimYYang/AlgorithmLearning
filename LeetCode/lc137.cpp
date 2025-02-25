208ff24c38dd41fc390b346f2691368c4596b5b6
JimYYang <jimsonnal@gmail.com>
2025-02-25 09:51:34 +0800

    lc137

--- LeetCode/lc137.cpp
+++ LeetCode/lc137.cpp
@@ -4,13 +4,12 @@ public:
         int res = 0;
         for (int i = 0; i < 32; i++)
         {
-            int cur_bit = 0;
+            int curBitCnt = 0;
             for (auto num : nums)
             {
-                cur_bit += (num >> i) & 1;
+                curBitCnt += num >> i & 1;
             }
-
-            res |= (cur_bit % 3) << i;
+            res |= (curBitCnt % 3) << i;
         }
         return res;
     }
