3d9446641e9eecf3e742cd4d98a130d48236d2b7
JimYYang <jimsonnal@gmail.com>
2025-03-12 17:45:39 +0800

    lc135

--- LeetCode/lc135.cpp
+++ LeetCode/lc135.cpp
@@ -2,22 +2,24 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> f(n, -1);
 
-        function<int(int)> dp = [&](int x) -> int {
-            if (f[x] != -1) return f[x];
+        vector<int> memo(n + 1, -1);
 
-            f[x] = 1;
-            if (x > 0 && ratings[x] > ratings[x - 1])
-                f[x] = max(f[x], dp(x - 1) + 1);
-            if (x + 1 < n && ratings[x] > ratings[x + 1])
-                f[x] = max(f[x], dp(x + 1) + 1);
-            return f[x]; 
+        function<int(int)> dp = [&](int x) -> int{
+            int &ref = memo[x];
+            if (ref != -1) return ref;
+            ref = 1;
+
+            if (x > 0 && ratings[x] > ratings[x - 1]) ref = max(ref, dp(x - 1) + 1); 
+            if (x + 1 < n && ratings[x] > ratings[x + 1]) ref = max(ref, dp(x + 1) + 1); 
+
+            return ref;
         };
 
         int res = 0;
         for (int i = 0; i < n; i++) res += dp(i);
-
         return res;
+
     }
+
 };
