b38fac5c78083ed4ef919456a6b96316042eb8a2
JimYYang <jimsonnal@gmail.com>
2025-03-12 16:01:55 +0800

    lc188

--- LeetCode/lc188.cpp
+++ LeetCode/lc188.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> f(n + 1, vector<vector<int>>(k + 1, vector<int>(2, -1e9)));
-        for (int i = 0; i <= n; i++) f[i][0][0] = 0;
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(k + 1, vector<int>(2, -2e9)));
+        for (int i = 0; i <= n; i++) dp[i][0][0] = 0;
 
         for (int i = 1; i <= n; i++)
         {
             for (int j = 1; j <= k; j++)
             {
-                f[i][j][0] = max(f[i - 1][j][0], f[i - 1][j][1] + prices[i - 1]);
-                f[i][j][1] = max(f[i - 1][j][1], f[i - 1][j - 1][0] - prices[i - 1]);
+                dp[i][j][0] = max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i - 1]);
+                dp[i][j][1] = max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i - 1]);
             }
         }
 
         int res = 0;
-        for (int i = 0; i <= k; i++) res = max(res, f[n][i][0]);
+        for (int i = 0; i <= k; i++) res = max(res, dp[n][i][0]);
         return res;
     }
 };
