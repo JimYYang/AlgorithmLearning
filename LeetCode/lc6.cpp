94f7a31471c1fc69af7491fe3f58613ab53c49c3
JimYYang <jimsonnal@gmail.com>
2025-03-13 20:11:34 +0800

    lc6

--- LeetCode/lc6.cpp
+++ LeetCode/lc6.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     string convert(string s, int n) {
-        if (n == 1)
-            return s;
-        int m = s.size();
+        if (n == 1) return s;
+        int len = s.size();
         string res;
-        for (int i = 0; i < n; i++) {
-            if (!i || i == n - 1) {
-                for (int j = i; j < m; j += 2 * n - 2)
+        for (int i = 0; i < n;i ++)
+        {
+            if (i == 0 || i == n - 1)
+            {
+                for (int j = i; j < len; j += 2 * n - 2)
+                {
                     res += s[j];
+                }
             }
-            else {
-                for (int j = i, k = 2 * n - 2 - i; j < m || k < m; j += 2 * n - 2, k += 2 * n - 2) {
-                    if (j < m)
-                        res += s[j];
-                    if (k < m)
-                        res += s[k];  
+            else
+            {
+                for (int j = i, k = 2 * n - 2 - i; j < len || k < len; j += 2 * n - 2, k += 2 * n - 2)
+                {
+                    if (j < len) res += s[j];
+                    if (k < len) res += s[k];
                 }
             }
         }
