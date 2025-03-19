f4b4b0163793134fe17ee1f2b0a5170513510aab
JimYYang <jimsonnal@gmail.com>
2025-03-19 14:38:48 +0800

    lc114

--- LeetCode/lc114.cpp
+++ LeetCode/lc114.cpp
@@ -12,14 +12,15 @@
 class Solution {
 public:
     void flatten(TreeNode* root) {
-        if (!root) return;
+        // 如果有左子树，将左子树右链插入当前节点和右子树之间
+        // 其实这一步就是将左子树插入到了根和右子树之间，所以满足前序遍历
+        // 如果没有左子树，则遍历右子树
         while (root)
         {
             auto p = root->left;
             if (p)
             {
-                while (p->right)
-                    p = p->right;
+                while (p->right) p = p->right;
                 p->right = root->right;
                 root->right = root->left;
                 root->left = nullptr;
