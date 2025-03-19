b919b89f36bfac9578b802060fc13428477a7fc9
JimYYang <jimsonnal@gmail.com>
2025-03-19 22:46:15 +0800

    lc113

--- LeetCode/lc113.cpp
+++ LeetCode/lc113.cpp
@@ -11,31 +11,30 @@
  */
 class Solution {
 public:
-    vector<vector<int>>ans;
+    vector<vector<int>> ans;
     vector<int> path;
-    vector<vector<int>> pathSum(TreeNode* root, int sum) {
-        if (root) dfs(root, sum);
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        if (root) dfs(root, targetSum);
         return ans;
     }
 
     void dfs(TreeNode *root, int sum)
     {
+        if (!root) return;
         path.push_back(root->val);
-
         sum -= root->val;
-
         if (!root->left && !root->right)
         {
-            if (!sum) ans.push_back(path);
-        }
-        else
-        {
-            if (root->left)
-                dfs(root->left, sum);
-            if (root->right)
-                dfs(root->right, sum);
+            if (!sum)
+            {
+                ans.push_back(path);
+            }
         }
 
+        if (root->left) dfs(root->left, sum);
+        if (root->right) dfs(root->right, sum);
+
+        // 只有外部变量需要“恢复原状态”
         path.pop_back();
     }
 };
