6371e3a41b71cad4bc78340c087868568cf033c4
JimYYang <jimsonnal@gmail.com>
2025-03-19 21:53:09 +0800

    lc104

--- LeetCode/lc104.cpp
+++ LeetCode/lc104.cpp
@@ -11,8 +11,24 @@
  */
 class Solution {
 public:
-    int maxDepth(TreeNode* root) {
-        if (!root) return 0;
-        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n = preorder.size();
+        unordered_map<int, int> pos;
+        for (int i = 0; i < n; i++)
+            pos[inorder[i]] = i;
+        
+        function<TreeNode*(int, int, int, int)> build = [&](int pl, int pr, int il, int ir) -> TreeNode* {
+            if (pl > pr) return nullptr;
+
+            int rootVal = preorder[pl];
+
+            int k = pos[rootVal];
+            TreeNode *root = new TreeNode(rootVal);
+
+            root->left = build(pl + 1, pl + k - il, il, k - 1);
+            root->right = build(pl + k - il + 1, pr, k + 1, ir);
+            return root;
+        };
+        return build(0, n - 1, 0, n - 1);
     }
 };
