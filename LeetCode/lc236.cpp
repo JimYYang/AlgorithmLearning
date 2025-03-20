869af761dd96a00e9c6e06041f62fe89f29d06ff
JimYYang <jimsonnal@gmail.com>
2025-03-20 10:45:19 +0800

    lc236

--- LeetCode/lc236.cpp
+++ LeetCode/lc236.cpp
@@ -10,21 +10,13 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        // 如果以root为根的子树 包含p和q，返回LCA
-        // 如果只包含p, 返回p
-        // 如果只包含q, 返回q
-        // 如果都不包含, 返回nullptr
-
+        // 以root为根的子树，如果包含LCA，返回LCA,如果只包含p返回p,如果只包含q，返回q
         if (!root || root == p || root == q) return root;
         auto left = lowestCommonAncestor(root->left, p, q);
         auto right = lowestCommonAncestor(root->right, p, q);
-        // 1. 如果左边为空, 那么右边： 
-        // a. 右边包含p,q 则右边是正确的LCA
-        // b. 右边只包含一个, 则返回p或q
-        // c. 右边都不包含, 返回nullptr
-        // 所以如果左边为空，都是返回right
         if (!left) return right;
         if (!right) return left;
+        // 都不空 说明一边一个  说明当前root就是LCA
         return root;
     }
 };
