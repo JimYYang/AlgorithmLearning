891f2dddf228615b9c324680dc22d16866c48a3b
JimYYang <jimsonnal@gmail.com>
2025-03-21 10:42:32 +0800

    lc47

--- LeetCode/lc47.cpp
+++ LeetCode/lc47.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    vector<int> path;
-    vector<bool> used;
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         int n = nums.size();
-        path.resize(n);
-        used.resize(n);
-        dfs(nums, 0);
+        vector<bool> used(n);
+        vector<int> path;
+        path.reserve(n);
+        vector<vector<int>> ans;
+
+        function<void(int)> dfs = [&](int u) 
+        {
+            if (u == n)
+            {
+                ans.push_back(path);
+                return;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (!used[i])
+                {
+                    if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
+                    used[i] = true;
+                    path.push_back(nums[i]);
+                    dfs(u + 1);
+                    path.pop_back();
+                    used[i] = false;
+                }
+            }
+        };
+        // 注意 数组不一定是有序的
+        sort(nums.begin(), nums.end());
+        dfs(0);
         return ans;
     }
-
-    void dfs(vector<int> &nums, int u)
-    {
-        if (u == nums.size())
-        {
-            ans.push_back(path);
-            return;
-        }
-
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (!used[i])
-            {
-                if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
-                used[i] = true;
-                path[u] = nums[i];
-                dfs(nums, u + 1);
-                used[i] = false;
-            }
-        }
-    }
-
-
 };
