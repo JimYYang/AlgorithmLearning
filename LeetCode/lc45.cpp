568bfab74136654fea190e028d4ecf93ecb5ea19
JimYYang <jimsonnal@gmail.com>
2025-03-22 21:52:25 +0800

    lc45

--- LeetCode/lc45.cpp
+++ LeetCode/lc45.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int start = 0, end = 1, n = nums.size(), maxPos = 0;
-        int res = 0;
-        while (end < n)
+        int n = nums.size();
+        int end = 0, step = 0, maxPos = 0;
+        // 不需要访问最后一个位置，因为如果访问前，最远一定可以到最后一个位置，如果再访问，边界刚好为最后一个位置，会多条一次
+        // 比如 1211z这个例子
+        for (int i = 0; i < n - 1; i++)
         {
-            for (int i = start; i < end; i++)
+            maxPos = max(maxPos, nums[i] + i);
+            if (i == end)
             {
-                maxPos = max(maxPos, nums[i] + i);
+                step++;
+                end = maxPos;
             }
-            res++;
-            start = end;
-            end = maxPos + 1;
         }
-        return res;
+        return step;
     }
 };
