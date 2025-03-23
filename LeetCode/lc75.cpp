b1c2501d4ba73d3066a879c4e0678fdfa8a351bd
JimYYang <jimsonnal@gmail.com>
2025-03-23 14:50:10 +0800

    lc75

---
+++ LeetCode/lc75.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i = 0, j = 0, k = nums.size() - 1;
+        for (; i <= k;)
+        {
+            if (nums[i] == 0)
+            {
+                swap(nums[i++], nums[j++]);
+            }
+            else if(nums[i] == 2)
+            {
+                swap(nums[i], nums[k--]);
+            }
+            else i++;
+        }
+    }
+};
