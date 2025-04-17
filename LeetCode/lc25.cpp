80c3d75cde1f08ef3230f60de2128aeac3a35aba
JimYYang <jimsonnal@gmail.com>
2025-04-17 19:37:52 +0800

    lc25

--- LeetCode/lc25.cpp
+++ LeetCode/lc25.cpp
@@ -1,4 +1,4 @@
-i/**
+/**
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
@@ -11,31 +11,27 @@ i/**
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode *dummy = new ListNode(-1, head);
-        ListNode *p = dummy;
+        auto dummy = new ListNode(-1, head);
 
+        auto p = dummy;
         while (true)
         {
-            ListNode *q = p;
-            for (int i = 0; i < k && q; i++)
-            {
-                q = q->next;
-            }
-            if (q == nullptr)
-                break;
-            ListNode *a = p->next, *b = a->next;
+            auto q = p;
+            for (int i = 0; i < k && q; i++) q = q->next;
+            if (!q) break;
+            auto a = p->next, b = a->next;
             for (int i = 0; i < k - 1; i++)
             {
-                ListNode *bNext = b->next;
+                auto bNext = b->next;
                 b->next = a;
                 a = b;
                 b = bNext;
             }
-            ListNode *pNext = p->next;
+            auto pNext = p->next;
             p->next = a;
             pNext->next = b;
             p = pNext;
         }
         return dummy->next;
     }
-}
+};
