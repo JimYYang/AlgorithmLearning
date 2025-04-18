fafba43664fb7e6ed8faf013fdb94cbd6ef27180
JimYYang <jimsonnal@gmail.com>
2025-04-18 11:08:50 +0800

    lc40

--- LeetCode/lc40.cpp
+++ LeetCode/lc40.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        // 包含重复元素 一次
+        sort(candidates.begin(), candidates.end());
 
-        vector<vector<int>> ans;
+        vector<vector<int>> res;
         vector<int> path;
         int n = candidates.size();
-
-        function<void(int, int)> dfs = [&](int u, int left) {
+        function<void(int, int)> dfs = [&](int left,  int u) -> void
+        {
             if (left == 0)
             {
-                ans.push_back(path);
+                res.push_back(path);
                 return;
             }
-
+            
+            // 这个题也可以用复杂的方法  枚举每个数填几次
             for (int i = u; i < n; i++)
             {
-                if (candidates[i] > left) continue;
+                if (candidates[i] > left) break;
+                // 在当前层 不用相同的数字
                 if (i > u && candidates[i] == candidates[i - 1]) continue;
-
                 path.push_back(candidates[i]);
-                dfs(i + 1, left - candidates[i]);
+                dfs(left - candidates[i], i + 1);
                 path.pop_back();
             }
         };
-        sort(candidates.begin(), candidates.end());
-        dfs(0, target);
-        return ans;
+        dfs(target, 0);
+        return res;
     }
 };
